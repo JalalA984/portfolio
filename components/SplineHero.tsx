22f7679e6227b5503fee90970da1bc96e566e41f
Jalal <128756237+JalalA984@users.noreply.github.com>
2024-09-25 12:35:53 -0400

    change spline scene url and add to globals.css for responsive spline

--- components/SplineHero.tsx
+++ components/SplineHero.tsx
@@ -1,18 +1,20 @@
 "use client";
 // @ts-ignore
 import Spline from "@splinetool/react-spline/next";
+import '../app/globals.css';
 
 // Spline design credits due to @mkhtr7
 // https://app.spline.design/@mkhtr7
 
 export default function SplineHero() {
-
   return (
     <div className="relative w-screen h-screen z-0">
-      <Spline
-        scene="https://prod.spline.design/gA6rZCmuKpLnFw5z/scene.splinecode"
-      />
-
+      <div className="w-full h-full">
+        <Spline
+          scene="https://prod.spline.design/EXf7Cw1kTSSAlKcM/scene.splinecode"
+          className="responsive-spline"
+        />
+      </div>
     </div>
   );
 }
