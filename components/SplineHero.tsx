418b4ed7902eda4c52b3cb55f989311b64622a73
Jalal <128756237+JalalA984@users.noreply.github.com>
2024-09-30 12:57:32 -0400

    remove broken webGL condition rendering

--- components/SplineHero.tsx
+++ components/SplineHero.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+
 // @ts-ignore
 import Spline from "@splinetool/react-spline/next";
 import "../app/globals.css";
@@ -7,35 +7,8 @@ import "../app/globals.css";
 // Spline design credits due to @mkhtr7
 // https://app.spline.design/@mkhtr7
 
-// Function to check for WebGL support
-function isWebGLAvailable() {
-  try {
-    const canvas = document.createElement("canvas");
-    return !!window.WebGLRenderingContext && 
-           (canvas.getContext("webgl") || canvas.getContext("experimental-webgl"));
-  } catch (e) {
-    return false;
-  }
-}
 
 export default function SplineHero() {
-  const [canRenderSpline, setCanRenderSpline] = useState(false);
-
-  useEffect(() => {
-    if (isWebGLAvailable()) {
-      setCanRenderSpline(true);
-    }
-  }, []);
-
-  if (!canRenderSpline) {
-    return (
-      <div className="w-full h-screen flex justify-center items-center">
-        {/* Optional Fallback Content */}
-        <p>3D content is not supported on your device.</p>
-      </div>
-    );
-  }
-
   return (
     <div className="relative w-screen h-screen z-0">
       <div className="w-full h-full">
