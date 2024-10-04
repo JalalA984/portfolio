c86a7a2126a1f9de2da197ea888005953fa67cbe
Jalal <128756237+JalalA984@users.noreply.github.com>
2024-10-04 08:51:09 -0400

    fix metadata exports

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,14 @@ import { Analytics } from "@vercel/analytics/react"
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter, Jost } from "next/font/google";
-import Head from "next/head";
 
-const inter = Inter({ subsets: ["latin"] });
 const jost = Jost({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Jalal • Developer Portfolio",
+  description: "Jalal Ahmad is an aspiring full-stack developer software engineer at the University of Maryland College Park. Jalal Ahmad is a Computer Science student graduating in May 2026 from the University of Maryland College Park. Passionate about software engineering, programming, Artificial Intelligence, Machine Learning, and full-stack development, Jalal is eager to build innovative solutions in tech. Connect with him on LinkedIn to learn more.",
+};
+
 
 export default function RootLayout({
   children,
@@ -14,14 +18,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <Head>
-        {/* Primary Meta Tags */}
-        <meta name="title" content="Jalal Ahmad • Developer Portfolio" />
-        <meta name="description" content="Jalal Ahmad is an aspiring full-stack developer software engineer at the University of Maryland College Park. Jalal Ahmad is a Computer Science student graduating in May 2026 from the University of Maryland College Park. Passionate about software engineering, programming, Artificial Intelligence, Machine Learning, and full-stack development, Jalal is eager to build innovative solutions in tech. Connect with him on LinkedIn to learn more." />
-
-        {/* LinkedIn Profile */}
-        <meta property="og:see_also" content="https://linkedin.com/in/jalalahm/" />
-      </Head>
       <body className={`${jost.className}`}>
         <Header />
         {children}
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import SectionDivider from "@/components/SectionDivider";
 import Contact from "@/components/Contact";
 import Coursework from "@/components/Coursework";
 import Footer from "@/components/Footer";
-import Head from "next/head";
 
 const SplineHero = dynamic(
   () => import("../components/SplineHero.tsx").then((mod) => mod.default),
@@ -16,19 +15,14 @@ const SplineHero = dynamic(
   }
 );
 
+export const metadata = {
+  keywords: "Jalal Ahmad, UMD, University of Maryland, College Park, Software Engineer, Software Engineering, Computer Science, Full Stack Developer, Programming, Artificial Intelligence, AI, Machine Learning, Jalal Ahmad Linkedin",
+};
+
+
 export default function Home() {
   return (
     <main className="h-screen">
-      <Head>
-        <title>Jalal Ahmad • Developer Portfolio</title>
-        <meta name="robots" content="index, follow" />
-        <meta
-          name="keywords"
-          content="Jalal Ahmad, UMD, University of Maryland, College Park, Software Engineer, Software Engineering, Computer Science, Full Stack Developer, Programming, Artificial Intelligence, AI, Machine Learning, Jalal Ahmad Linkedin"
-        />
-        <link rel="canonical" href="https://jalalahmad.vercel.app/" />
-      </Head>
-
       {/* SplineHero is dynamically imported and will only be rendered client-side */}
       <SplineHero />
       <section className="flex flex-col justify-center items-center mx-auto my-auto">
