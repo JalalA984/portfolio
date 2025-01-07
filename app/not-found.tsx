225f066b5f395341efde4e130b62c1a2f76e91cd
Jalal <128756237+JalalA984@users.noreply.github.com>
2025-01-06 19:14:12 -0500

    fix responsive 404

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -17,8 +17,8 @@ const NotFound = () => {
   return (
     <section className="w-full h-screen flex justify-center items-center bg-[#E4E4E4] overflow-hidden relative">
       <div className="absolute inset-0 flex justify-center items-center">
-        <h1 className="text-[10rem] sm:text-[14rem] md:text-[18rem] lg:text-[22rem] font-bold text-center text-black">
-          4 <span className="text-transparent stroke-4 text-cyan-200">X</span> 4
+        <h1 className="text-[6rem] sm:text-[8rem] md:text-[12rem] lg:text-[16rem] xl:text-[20rem] font-bold text-center text-black">
+          4 <span className="text-cyan-500">X</span> 4
         </h1>
       </div>
       <div
@@ -27,9 +27,7 @@ const NotFound = () => {
       ></div>
 
       <Link href="/" className="absolute bottom-10 text-2xl bg-cyan-500 text-white py-2 px-6 rounded-md shadow-lg hover:bg-cyan-600 transition duration-300">
-        
-          Go Back Home
-        
+        Go Back Home
       </Link>
     </section>
   );
